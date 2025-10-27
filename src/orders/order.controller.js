2fd305eb7af16792342201a5ba45b1cffba38bd7
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2025-10-27 10:04:16 +0600

    get orders by email

--- src/orders/order.controller.js
+++ src/orders/order.controller.js
@@ -1,5 +1,6 @@
 const { BASE_URL } = require("../utils/baseURL");
-const { errorResponse } = require("../utils/responseHandler");
+const { errorResponse, successResponse } = require("../utils/responseHandler");
+const Order = require("./order.model");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -30,6 +31,58 @@ const makePaymentReq = async (req, res) => {
   }
 };
 
+const confirmPayment = async (req, res) => {
+  const { session_id } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.retrieve(session_id, {
+      expand: ["line_items", "payment_intent"],
+    });
+    const paymentIntentId = session.payment_intent.id;
+    let order = await Order.findOne({ orderId: paymentIntentId });
+    if (!order) {
+      const lineItems = session.line_items.data.map((item) => ({
+        productId: item.price.product,
+        quantity: item.quantity,
+      }));
+      const amount = session.amount_total / 100;
+      order = new Order({
+        orderId: paymentIntentId,
+        products: lineItems,
+        amount: amount,
+        email: session.customer_details.email,
+        status:
+          session.payment_intent.status === "succeeded" ? "Pending" : "Failed",
+      });
+    } else {
+      order.status =
+        session.payment_intent.status === "succeeded" ? "Pending" : "Failed";
+    }
+    await order.save();
+    return successResponse(res, 200, "Order Confirmed Successfully", order);
+  } catch (error) {
+    return errorResponse(res, 500, "Error on confirm payment", error);
+  }
+};
+
+const getOrdersByEmail = async (req, res) => {
+  const email = req.params.email;
+  try {
+    if (!email) {
+      return errorResponse(res, 400, "Email is required");
+    }
+    const orders = await Order.find({ email }).sort({ createdAt: -1 });
+    if (orders.length === 0 || !orders) {
+      return errorResponse(res, 404, "Orders Not Found");
+    }
+    return successResponse(res, 200, "Orders Get Successfully", orders);
+  } catch (error) {
+    errorResponse(res, 500, "Failed to get orders by email", error);
+  }
+};
+
 module.exports = {
   makePaymentReq,
+  confirmPayment,
+  getOrdersByEmail,
 };
--- src/orders/order.route.js
+++ src/orders/order.route.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { makePaymentReq } = require("./order.controller");
+const { makePaymentReq, confirmPayment, getOrdersByEmail } = require("./order.controller");
 const router = express.Router();
 
 // create checkout
-router.post("/create-checkout-session", makePaymentReq)
+router.post("/create-checkout-session", makePaymentReq);
+
+// confirm payment
+router.post("/confirm-payment", confirmPayment);
+
+
+// get orders by email 
+router.get("/:email" , getOrdersByEmail)
 
 module.exports = router;
