e52311e22a4f812c308619ea2484202d5dcbce6e
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2025-10-27 10:05:31 +0600

    ggg

--- src/orders/order.route.js
+++ src/orders/order.route.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { makePaymentReq, confirmPayment, getOrdersByEmail } = require("./order.controller");
+const {
+  makePaymentReq,
+  confirmPayment,
+  getOrdersByEmail,
+} = require("./order.controller");
 const router = express.Router();
 
 // create checkout
@@ -8,8 +12,9 @@ router.post("/create-checkout-session", makePaymentReq);
 // confirm payment
 router.post("/confirm-payment", confirmPayment);
 
+// get orders by email
+router.get("/:email", getOrdersByEmail);
 
-// get orders by email 
-router.get("/:email" , getOrdersByEmail)
+
 
 module.exports = router;
